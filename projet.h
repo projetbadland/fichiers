387ba423ac4b037b7b7131d3ff1060aab1b18e8c
alexandredeschaume <alex.desch45@hotmail.fr>
2014-03-28 15:28:30 +0100

    Update projet.h

--- projet.h
+++ projet.h
@@ -7,8 +7,24 @@
 #include <string.h>
 #define NBPARA 3
 
-int jeu(int niveau);
+#ifndef H_PROTO
+#define H_PROTO
+
+//perso.c
+void ajoutPerso(t_perso **ancre, float perso_px, float perso_py, float perso_vx, float perso_vy, float perso_ax, float perso_ay, int perso_deg);
+void supprimerPerso(t_perso **ancre, t_perso *perso_mort);
+void mouvPerso(t_perso *ancre, float para[NBPARA]);
+
+//jeu.c
+void jeu(int niveau);
+
+//initialisation.c
 void lancerAllegro(int largeur, int hauteur);
 void chargement(int niveau,int **paraBM ,BITMAP *fond, BITMAP *coll, BITMAP *calque);
+
+//affichage.c
+void affichage(float vitesseScroll, BITMAP *fond, BITMAP *coll, BITMAP *calque);
+
+#endif // H_PROTO
 
 #endif // PROJET_H_INCLUDED
